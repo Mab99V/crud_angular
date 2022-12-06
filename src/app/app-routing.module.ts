750b5519ace60b9bf8870375499172d1c8280f7a
Mab99V <mabelperezgaribay@gmail.com>
2022-12-05 20:29:12 -0600

    crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login/login.component';
+import { RegistroComponent } from './auth/registro/registro/registro.component';
+import { EmpleadoListComponent } from './empleado-list/empleado-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'registro',component:RegistroComponent},
+  {path: 'crud',component:EmpleadoListComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EmpleadoListComponent } from './empleado-list/empleado-list.component';
+import { FormsModule, ReactiveFormsModule } from  '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login/login.component';
+import { RegistroComponent } from './auth/registro/registro/registro.component';
+import { AddEmpleadosComponent } from './addEmpleados/add-empleados/add-empleados.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    EmpleadoListComponent,
+    LoginComponent,
+    RegistroComponent,
+    AddEmpleadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
